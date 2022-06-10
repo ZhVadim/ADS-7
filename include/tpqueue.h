793c40cf756471719cdad11e997725a4a6fc2c3d
Вадим Желтов <99135277+ZhVadim@users.noreply.github.com>
2022-06-10 21:08:31 +0300

    B106

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,50 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct ITEM {
+        T value;
+        ITEM* next;
+    };
+    ITEM* head;
+    ITEM* create(const T& value) {
+        ITEM* item = new ITEM;
+        item->value = value;
+        item->next = nullptr;
+        return item;
+    }
+ public:
+    TPQueue() : head(nullptr) { }
+    void push(const T& item) {
+        if (item.prior > head->value.prior) {
+            ITEM* nitem = create(item);
+            nitem->next = head;
+            head = nitem;
+            return;
+        }
+        if (!head) {
+            head = create(item);
+            return;
+        }
+        ITEM* temp = head;
+        while (temp->next) {
+            if (item.prior > temp->next->value.prior) {
+                ITEM* nitem = create(item);
+                nitem->next = temp->next;
+                temp->next = nitem;
+                return;
+            }
+            temp = temp->next;
+        }
+        temp->next = create(item);
+    }
+    T pop() {
+        ITEM* nhead = head->next;
+        T tmp = head->value;
+        delete head;
+        head = nhead;
+        return tmp;
+    }
 };
 
 struct SYM {
